8d19c038fc83ac7daf7c4ae25a1c17522cc2cf8b
lanvalird <strazhognay@gmail.com>
2025-09-09 18:43:12 +0400

    add content docs: builds info; update favicon

--- src/content/_meta.ts
+++ src/content/_meta.ts
@@ -1,7 +1,19 @@
 export default {
+  "*": {
+    theme: {
+      collapsed: true,
+    },
+  },
   index: {},
   "getting-started": {},
   features: {
     title: "Возможности",
+  },
+  _tech: {
+    title: "Техническое",
+    type: "separator",
+  },
+  builds: {
+    title: "Сборки игры",
   },
 };
---
+++ src/content/builds/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  latest: {},
+  "nelya-1.x": {},
+  "sofia-1.x": {},
+};
